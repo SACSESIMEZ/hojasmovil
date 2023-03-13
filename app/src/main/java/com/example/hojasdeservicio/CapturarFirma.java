0a3fc88180644cf10da04582870f310c6472ca37
SACSESIMEZ <sacs.udiesimez@gmail.com>
2023-03-13 14:54:56 -0600

    Primer commit

--- app/src/main/java/com/example/hojasdeservicio/CapturarFirma.java
+++ app/src/main/java/com/example/hojasdeservicio/CapturarFirma.java
@@ -3,6 +3,7 @@ package com.example.hojasdeservicio;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
@@ -15,9 +16,20 @@ public class CapturarFirma extends View {
     private Bitmap _bitmap;
     private Canvas _canvas;
     private Path _path;
-    private Paint _bitmapPaint;
+    private Paint _bitmapPaint, _paint;
+    private float _mX, _mY, _touchTolerancia = 4, _lineaEspesor = 4;
 
     public CapturarFirma(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        _path = new Path();
+        _bitmapPaint = new Paint(Paint.DITHER_FLAG);
+        _paint = new Paint();
+        _paint.setAntiAlias(true);
+        _paint.setDither(true);
+        _paint.setColor(Color.BLACK);
+        _paint.setStyle(Paint.Style.STROKE);
+        _paint.setStrokeJoin(Paint.Join.ROUND);
+        _paint.setStrokeCap(Paint.Cap.ROUND);
+        _paint.setStrokeWidth(_lineaEspesor);
     }
 }
